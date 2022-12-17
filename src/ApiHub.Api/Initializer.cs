e920611c976e0427a6edc1fb5d6496c7ea4c6ddf
Liam Sho <conan_creeper@outlook.com>
2022-12-17 17:46:28 +0800

    feat: add configuration and logger

---
+++ src/ApiHub.Api/Initializer.cs
@@ -0,0 +1,44 @@
+// This file is a part of ApiHub project.
+// ApiHub belongs to the MAA organization.
+// Licensed under the AGPL-3.0 license.
+
+using Com.Ctrip.Framework.Apollo;
+using Com.Ctrip.Framework.Apollo.Enums;
+using Serilog;
+using ILogger = Serilog.ILogger;
+
+namespace ApiHub.Api;
+
+public static class Initializer
+{
+    public static IConfigurationRoot GetConfiguration()
+    {
+        var builder = new ConfigurationBuilder();
+        
+        if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("APOLLO_DISABLED")))
+        {
+            builder.AddApollo(new ApolloOptions
+                {
+                    AppId = Environment.GetEnvironmentVariable("APOLLO_APP_ID") ?? "api-hub",
+                    MetaServer = Environment.GetEnvironmentVariable("APOLLO_META_SERVER") ?? "http://localhost:8080",
+                    Cluster = Environment.GetEnvironmentVariable("APOLLO_CLUSTER") ?? "default",
+                    Namespaces = Environment.GetEnvironmentVariable("APOLLO_NAMESPACES")?.Split(',') ?? new[] { "application" },
+                    Secret = Environment.GetEnvironmentVariable("APOLLO_SECRET"),
+                    Env = Enum.TryParse<Env>(Environment.GetEnvironmentVariable("APOLLO_ENV"), out var env) ? env : Env.Dev
+                });
+        }
+        else
+        {
+            builder.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+        }
+        
+        return builder.Build();
+    }
+
+    public static ILogger GetLogger(IConfigurationRoot configuration)
+    {
+        return new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+    }
+}
--- src/ApiHub.Api/Program.cs
+++ src/ApiHub.Api/Program.cs
@@ -2,10 +2,41 @@
 // ApiHub belongs to the MAA organization.
 // Licensed under the AGPL-3.0 license.
 
+using ApiHub.Api;
+using Microsoft.AspNetCore.HttpOverrides;
+using Serilog;
+using Serilog.Debugging;
+
+var configuration = Initializer.GetConfiguration();
+var logger = Initializer.GetLogger(configuration);
+
+Log.Logger = logger;
+SelfLog.Enable(Console.Error);
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddConfiguration(configuration);
+
+builder.Host.UseSerilog();
+
+builder.Services.AddCors();
+builder.Services.AddControllers();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.All;
+});
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseForwardedHeaders();
+app.UseCors(options =>
+{
+    options.SetIsOriginAllowed(_ => true)
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials();
+});
+
+app.MapControllers();
 
 app.Run();
